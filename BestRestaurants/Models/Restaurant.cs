0b22d8f96b9e52b6a49b982c7b4c1c4b3162a3ed
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-29 13:56:45 -0700

    add Cuisine property
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>
    co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -6,6 +6,6 @@ namespace BestRestaurants
     public int CuisineId { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
-    public virtual Restaurant Restaurant { get; set; }
+    public virtual Cuisine Cuisine { get; set; }
   }
 }
