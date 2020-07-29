3f8101cf072ba74682f0947328010a0ec9d3093b
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-29 11:14:51 -0700

    add BestRestaurantContext class
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>
    co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

---
+++ BestRestaurants/Models/BestRestaurantsContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BestRestaurants.Models
+{
+  public class BestRestaurantsContext : DbContext
+  {
+    public virtual DbSet<Cuisine> Cuisine { get; set; }
+
+    public DbSet<Restaurant> Restaurants { get; set; }
+
+    public BestRestaurantsContext(DbContextOptions options) : base(options) { }
+  }
+}
+
